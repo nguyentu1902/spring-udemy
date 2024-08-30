3d9248b32fcc10da076ae6cc13609181b0c9c622
202282-NguyenTu <sing19022001@gmail.com>
2024-08-30 13:18:47 +0700

    empty

--- prj-chap4/cruddemo-data-rest/src/main/java/com/cruddemo/cruddemo/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cruddemo.cruddemo.repository;
-
-import com.cruddemo.cruddemo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
--- prj-chap4/demo/src/main/java/com/restdemo/demo/exception/StudentErrorResponse.java
+++
@@ -1,9 +0,0 @@
-package com.restdemo.demo.exception;
-
-import lombok.Data;
-
-@Data
-public class StudentErrorResponse {
-    private int status;
-    private String message;
-}
